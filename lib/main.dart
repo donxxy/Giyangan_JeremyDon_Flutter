c89bd50931a16709cbde4d3a221ea6940a622db7
Jeremy Don Giyangan <jeremydon.giyangan-25@cpu.edu.ph>
2026-08-24 22:03:39 +0800

    LAB7

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,87 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'My First Flutter App',
-      home: const CounterScreen(),
-    ); // MaterialApp
+    return const MaterialApp(
+      title: 'Mini Playlist',
+      home: PlaylistScreen(),
+    );
   }
 }
 
-class CounterScreen extends StatefulWidget {
-  const CounterScreen({super.key});
+class PlaylistScreen extends StatelessWidget {
+  const PlaylistScreen({super.key});
 
-  @override
-  State<CounterScreen> createState() => _CounterScreenState();
-}
-
-class _CounterScreenState extends State<CounterScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-  void _decrementCounter() {
-    setState(() {
-      _counter--;
-    });
-  }
+  // Step 7a: The list of songs
+  final List<String> songs = const [
+    'Sunset Drive',
+    'Strategy (feat. Megan Thee Stallion)',
+    'Upuan by Gloc-9',
+    'Kumilos (feat. Higit Sa Pag-Ibig, The Musical)',
+    'Aint In LA by ADELA',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.deepPurple.shade50,
-      appBar: AppBar(
-        title: const Text('My First Flutter App'),
-        backgroundColor: Colors.deepPurple,
-        foregroundColor: Colors.white,
-      ), // AppBar
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text(
-                  'You have pressed the button this many times:',
-                ), // Text
-                Text(
-                  '$_counter',
-                  style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-                ), // Text
-              ],
-            ), // Column
-          ), // Center
-          const Align(
-            alignment: Alignment.bottomCenter,
-            child: Padding(
-              padding: EdgeInsets.only(bottom: 20.0),
-              child: Text(
-                'Created by: Jeremy Don Giyangan',
-                style: TextStyle(
-                  fontSize: 14,
-                  fontStyle: FontStyle.italic,
-                  color: Colors.deepPurple,
-                ), // TextStyle
-              ), // Text
-            ), // Padding
-          ), // Align
-        ],
-      ), // Stack
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: _incrementCounter,
-            heroTag: 'increment',
-            child: const Icon(Icons.add),
-          ), // FloatingActionButton
-          const SizedBox(height: 12),
-          FloatingActionButton(
-            onPressed: _decrementCounter,
-            heroTag: 'decrement',
-            backgroundColor: Colors.deepPurple.shade200,
-            child: const Icon(Icons.remove),
-          ), // FloatingActionButton
-        ],
-      ), // Column
-    ); // Scaffold
+      appBar: AppBar(title: const Text('My Playlist')),
+      // Step 7b: The ListView.builder
+      body: ListView.builder(
+        itemCount: songs.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: const Icon(Icons.music_note),
+            title: Text(songs[index]),
+            onTap: () {
+              // TODO 1 & 2: Calling Navigator.push and passing songs[index]
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => NowPlayingScreen(
+                    songTitle: songs[index],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class NowPlayingScreen extends StatelessWidget {
+  final String songTitle;
+  
+  const NowPlayingScreen({super.key, required this.songTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Now Playing')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Playing: $songTitle',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Stop and Go Back'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
